ff8329a9b3f8132c833cafc7ecc34b6c72ea70c4
sorrycc <sorrycc@gmail.com>
2023-03-04 09:51:29 +0800

    chore: update

--- test.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-(async () => {
-  const res = await axios.get('https://comment.bilibili.com/38442945.xml');
-  console.log(res.data);
-})().catch(e => {
-  console.error(e);
-});
