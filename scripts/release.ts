4d6937882497f177bd07cb3ccc9bc97f6041d256
sorrycc <sorrycc@gmail.com>
2023-03-05 20:46:28 +0800

    feat: add url-system bin file

---
+++ scripts/release.ts
@@ -0,0 +1,18 @@
+import 'zx/globals';
+import getGitRepoInfo from 'git-repo-info';
+
+(async () => {
+  const { branch } = getGitRepoInfo();
+  await $`npm run build`;
+  await $`npm version patch`;
+  const newVersion = require('../package.json').version;
+  await $`npm publish`;
+
+  // commit and tag and push
+  await $`git commit -am "release: ${newVersion}"`;
+  await $`git tag v${newVersion}`;
+  await $`git push origin ${branch} --tags`;
+})().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
