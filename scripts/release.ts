e0d99cd0a14e21c6c504769cfc27280dc27618b5
sorrycc <sorrycc@gmail.com>
2023-03-05 20:51:18 +0800

    chore: update release script

--- scripts/release.ts
+++ scripts/release.ts
@@ -3,14 +3,10 @@ import getGitRepoInfo from 'git-repo-info';
 
 (async () => {
   const { branch } = getGitRepoInfo();
+  await $`npm run doctor`;
   await $`npm run build`;
   await $`npm version patch`;
-  // const newVersion = require('../package.json').version;
   await $`npm publish`;
-
-  // commit and tag and push
-  // await $`git commit -am "release: ${newVersion}"`;
-  // await $`git tag v${newVersion}`;
   await $`git push origin ${branch} --tags`;
 })().catch((e) => {
   console.error(e);
